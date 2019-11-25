3ff1933ca186b336a20ea02cb66b4ba323560ad4
Cordes-RJ <richardj.cordes@gmail.com>
2019-11-25 17:12:34 -0500

    added decorator for dictionaries for ease of use and safety

---
+++ Code/utilMap.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+class Map:
+    def __init__(self, defaultReturn):
+        self.m = dict()
+        self.default = defaultReturn # what is returned in a failure to get key
+    def ListKeys(self):
+        return self.m.keys()
+    def InMap(self,key):
+        if key in self.m.keys():
+            return True
+    def SetOrAdd(self, key, value):
+        self.m[key] = value
+    def Del(self,key):
+        if self.InMap(key):
+            self.m.pop(key)
+    # set the default return to be used for now on
+    def setDefaultReturn(self,defaultReturn):
+        self.default = defaultReturn
+    # 
+    def Get(self,key):
+        if self.InMap(key):
+            return self.m[key]
+        return self.default
+        
+        
