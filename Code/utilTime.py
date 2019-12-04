b1acca0e7c99d44f910ef328ced638776d6d9456
Cordes-RJ <richardj.cordes@gmail.com>
2019-12-04 16:01:16 -0500

    added lap function to stopwatch

--- Code/utilTime.py
+++ Code/utilTime.py
@@ -8,9 +8,18 @@ from datetime import datetime
 class StopWatch:
     def __init__(self):
         self.time = time.time()
+        self.Lap = 0
     def stop(self):
         return time.time() - self.time
-    def getTimeString(self, roundTo):
+    def getLapTimeString(self, roundTo):
+        timeString = ""
+        if self.Lap == 0:
+            timeString = str(round(time.time() - self.time,roundTo)) + " seconds"
+        else:
+            timeString = str(round(time.time() - self.Lap,roundTo)) + " seconds"
+        self.Lap = time.time()
+        return timeString
+    def getFullTimeString(self,roundTo):
         return str(round(time.time() - self.time,roundTo)) + " seconds"
     
 def timeToDateString(timestamp):
@@ -23,3 +32,12 @@ def timeToDateString(timestamp):
 
 def getDateString():
     return timeToDateString(time.time())
+
+"""
+s = StopWatch()
+time.sleep(0.25)
+print(s.getLapTimeString(4))
+time.sleep(0.5)
+print(s.getLapTimeString(4))
+print(s.getFullTimeString(4))
+"""
