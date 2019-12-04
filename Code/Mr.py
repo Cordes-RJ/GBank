ecef10aa94b950a330539b47e5a48db618eb5398
Cordes-RJ <richardj.cordes@gmail.com>
2019-12-04 11:43:49 -0500

    Manager now does atomicity check before proceeding to other functions

--- Code/Mr.py
+++ Code/Mr.py
@@ -5,20 +5,26 @@ the many scripts and the flow of the program itself
 """
 import logger
 import parameters
+import atomicity
 import warehouse
 import bagnonSoup
 
 class Manager:
     def __init__(self):
-        logger.Start() # begin Logging
+        logger.Start()                                                          # begin Logging
         # instantiate parameters
-        self.paramLedger = parameters.Ledgerman() # instantiate parameters
-        #self.warehouseLedger = warehouse.Build(self.paramLedger.GetWarehousePath())
-    def Test(self):
-        return warehouse.Build(self.paramLedger.GetWarehousePath())
+        self.paramLedger = parameters.Ledgerman()                               # instantiate parameters
+        # ensure necessary files are available before continuing
+        if self.AtomicityCheck():                                               # for readability
+            pass
+        # build warehouse ledger
+        self.warehouseLedger = warehouse.Build(self.paramLedger.GetWarehousePath())
+        # update for bag contents with new counts
+    def AtomicityCheck(self):
+        return atomicity.Check(self.paramLedger)
     # BagUpdate grabs items from bagbrother and updates the ledger
     def BagUpdate(self):
         return bagnonSoup.Parser().Parse(self.paramLedger.GetBagPath(),self.paramLedger.characterWL)
 
 #%%
-m = Manager()
+#x = [Manager().BagUpdate()]
