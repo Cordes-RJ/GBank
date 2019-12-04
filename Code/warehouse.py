2169d2406e56f00eb03ca78363b0ae2fc8231433
Cordes-RJ <richardj.cordes@gmail.com>
2019-12-04 09:08:19 -0500

    added update functions and attribute to item class

--- Code/warehouse.py
+++ Code/warehouse.py
@@ -15,6 +15,7 @@ import wowhead
 class Item:
     def __init__(self):
         self.itemID = 0
+        self.new = True
         self.Name = ""
         self.Rarity = -1
         self.IconName = ""
@@ -23,25 +24,34 @@ class Item:
         self.Ct = 0
     def INITviaDFrow(self,dfRow):
         self.itemID = dfRow['itemID']
-        self.Name = (dfRow['Name']).encode()
+        self.Name = (dfRow['Name']).encode('utf-8')
         self.Rarity = dfRow['Rarity']
-        self.IconName = (dfRow['IconName']).encode()
-        print(dfRow['Type'] +"," +dfRow['Subtype'])
+        self.IconName = (dfRow['IconName']).encode('utf-8')
         self.itemRefCode = itemRef.getRefCodefromString(dfRow['Type'] +"," +dfRow['Subtype'])
         self.LastPrice = dfRow['LastPrice']
         self.Ct = 0
+        self.new = False # was found in a previous update
         return self
     def ToCSVrow(self):
         name = self.Name.decode('utf-8')
         typeAndSubtype = itemRef.getStringFromRefCode(self.itemRefCode)
         iconName = self.IconName.decode('utf-8')
         link = wowhead.makeDisplayURLfromItemID(self.itemID)
-        attList = [self.itemID,name,link,self.Rarity,iconName,typeAndSubtype,self.LastPrice,self.Ct]
+        attList = [self.itemID,name,link,self.Rarity,iconName,typeAndSubtype,self.LastPrice,self.Ct,self.ct * self.LastPrice]
         return utilFile.ListOfItemsToCSVRow(attList)
     def INITviaID(self, itemID):
         self.itemID = itemID
         return self
-
+    def UpdateWoWheadInfo(self, winfo):
+        winfo = wowhead.wowheadItemInfo()
+        self.Name = (winfo.Name).encode('utf-8')
+        self.Rarity = winfo.Rarity
+        self.itemRefCode = winfo.RefCode
+        self.IconName = (winfo.IconName).encode('utf-8')
+    def UpdatePrice(self, price):
+        self.LastPrice = price
+    def UpdateCt(self,ct):
+        self.Ct = ct
         
 
 # take raw CSV path and return Pandas dataframe
@@ -64,8 +74,8 @@ def dfRowToAttributeList(dfRow):
     return itemRow
     
 # returns dictionary of items 
-def Build():
-    df = getFileContents("test_warehouse.csv")
+def Build(Path):
+    df = getFileContents(Path)
     items = []
     for i,i2 in df.iterrows():
         items.append(Item().INITviaDFrow(i2))
@@ -74,7 +84,8 @@ def Build():
         itemDict.Add(item.itemID,item)
     return itemDict
         
-        
+
+
 """
 #x= Build()
 #for ID in x.ListKeys():
