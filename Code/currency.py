e836012ec4d59cb497e67933a174329c081871f2
Cordes-RJ <richardj.cordes@gmail.com>
2019-12-05 08:44:58 -0500

    added functions to deal with wow currency

---
+++ Code/currency.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class WoW:
+    def __init__(self,gold,silver,copper):
+        self.gold = int(gold)
+        self.silver= int(silver)
+        self.copper = int(copper)
+    def toString(self):
+        return ("g:"+str(self.gold)+"|"+str(self.silver)+"|"+str(self.copper))
+
+def IntToGold(integer):
+    copper = integer % 100
+    integer = (integer-copper)/100
+    silver = integer%100
+    gold = (integer-silver)/100
+    return WoW(gold,silver,copper)
+    
+def GoldFloat(integer):
+    return round(integer/10000,4)
